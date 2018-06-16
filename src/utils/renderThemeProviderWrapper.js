aa3aba88a522ea73130837a6c08d83bf067bda9a
alan mulhall <alanmulhall@gmail.com>
2018-06-16 10:56:01 +1000

    Added eslint

--- src/utils/renderThemeProviderWrapper.js
+++ src/utils/renderThemeProviderWrapper.js
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'styled-components';
 import { render } from 'enzyme';
 import theme from '../styled/theme';
 
-const renderThemeProviderWrapper = (children, theme) =>
+const renderThemeProviderWrapper = children =>
   render(<ThemeProvider theme={theme}>{children}</ThemeProvider>);
 
 export default renderThemeProviderWrapper;
